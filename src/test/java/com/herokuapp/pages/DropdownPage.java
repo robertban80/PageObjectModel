d06870cbf7fd5175b5fa156326004d01b6bd16b5
robert.ban <robertban80@gmail.com>
2023-02-22 20:15:57 +0200

    lab 22.02.2023

--- src/test/java/com/herokuapp/pages/DropdownPage.java
+++ src/test/java/com/herokuapp/pages/DropdownPage.java
@@ -1,12 +1,41 @@
 package com.herokuapp.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class DropdownPage extends BasePage{
     public final static String DROPDOWN_URL = BASE_URL + "dropdown";
 
+    @FindBy (id="dropdown") private WebElement dropdown;//am identificat elemetul de tip dropdown din pagina
+    Select select = new Select(dropdown); //tre sa fie de tip Select ca sa pot interactiona cu optiunile din dropdown
+
+    @FindBy (xpath = "//*[@id=\"dropdown\"]/option[2]") private WebElement firstOption;
+    @FindBy (xpath = "//*[@id=\"dropdown\"]/option[3]") private WebElement secondOption;
+
+
     public DropdownPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void selectFirstOption(){
+        dropdown.click();
+        firstOption.click();
+    }
+
+    public void selectOptions(String optionValue){
+        select.selectByVisibleText(optionValue);
+
+    }
+
+    public boolean checkThatFirstOptionIsSelected(){
+        return firstOption.isSelected();
+
+    }
+    public boolean checkThatOptionIsSelected(String selectedOption){
+        return (selectedOption.equals(secondOption.getText()));
+
     }
 
 
--- src/test/java/com/herokuapp/tests/DropdownTests.java
+++ src/test/java/com/herokuapp/tests/DropdownTests.java
@@ -1,16 +1,37 @@
 package com.herokuapp.tests;
 
 
-
+import com.herokuapp.pages.DropdownPage;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.herokuapp.pages.DropdownPage.DROPDOWN_URL;
-public class DropdownTests extends BaseTest{
 
-    @Test
-    public void dropdown(){
+public class DropdownTests extends BaseTest {
 
+    @Test(testName = "Select Option 1")
+    public void selectFirstOptionInDropdown() {
+        driver = new ChromeDriver();
+        driver.get(DROPDOWN_URL);
+
+        DropdownPage dropdownPage = new DropdownPage(driver); //am construit obiectul dropdownpage ca sa pot apela toate metodele din clasa dropdownpage
+        dropdownPage.selectFirstOption();
+        //dropdownPage.selectOptions("Option 1");
+        Assert.assertTrue(dropdownPage.checkThatFirstOptionIsSelected());
 
     }
+
+    @Test(testName = "Select Option 2")
+    public void selectSecondOptionInDropdown() {
+        driver = new ChromeDriver();
+        driver.get(DROPDOWN_URL);
+
+        DropdownPage dropdownPage = new DropdownPage(driver); //am construit obiectul dropdownpage ca sa pot apela toate metodele din clasa dropdownpage
+        dropdownPage.selectOptions("Option 2");
+        Assert.assertTrue(dropdownPage.checkThatOptionIsSelected("Option 2"));
+
+    }
+
 
 }
